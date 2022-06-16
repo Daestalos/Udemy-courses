a671066024f92a15bd84a93a401cc6ef4eb746b0
Daestalos <alex.bordyug.daes@gmail.com>
2022-06-16 23:25:47 +0300

    l44 p5 l47 l49

--- JS Lessons/L47 Рекурсия.js
+++ JS Lessons/L47 Рекурсия.js
@@ -42,9 +42,6 @@ poww(2,4); // 16
 // !!!!!!<--------------- Пример перебора объекта через рекурсию ------------------->
 
 // задача: посчитать средний прогресс со всех студентов, со всех курсов
-// 1) понять сколько вообще студентов со всех курсов
-// нам необходимо общее число в %
-// 2) берем общее число и делем на кол-во студентов 
 
 
 let students = {
@@ -72,17 +69,203 @@ let students = {
     }
 };
 
-// через цикл
+// через цикл (эффективно только при условии, что объект будет неизменным)
 
 function getTotalProgressByIteration (data) {
     let total = 0,
         students = 0;
 
+    for (let course of Object.values(data)){
+        if (Array.isArray(course)) {
+            students += course.length; // = 2
+            
+            for (let i = 0; i < course.length; i++){
+                total += course[i].progress // перебираем каждого студента в объекте
+            }
+        } else {
+            for (let subCourse of Object.values(course)){
+                students += subCourse.length;
 
+                for (let i = 0; i < subCourse.length; i++){
+                    total += subCourse[i].progress
+                }
+            }
+        }
+    }
 
     return total / students;
 }
 
 console.log(getTotalProgressByIteration(students));
 
+
 // через рекурсию
+
+function getTotalProgressByRecursion(data) { // запускаем функцию рекурсии
+    // в нее попадают какие-то данные, это может быть сразу массив
+    if (Array.isArray(data)) { // проверяем на массив
+        let total = 0; // задаем переменную
+        
+        for (let i = 0; i < data.length; i++){
+            total += data[i].progress; // складываем прогресс каждого из студентов при помощи цикла
+        }
+        // так как нам нужно рассчитывать сразу 2 значения, общий прогресс и количество студентов
+        return [total, data.length]; // берем и возвращаем сразу два значения
+    } else { // если же этот объект это не масив, то попадаем сюда
+        let total = [0, 0]; // формируем массив с нулями, чтобы туда записать оба значения
+        
+        for (let subData of Object.values(data)){ // так как это объект - перебираем его, так как нас название свойства не интересует, то берем его значения через Object.values
+            const subDataArray = getTotalProgressByRecursion(subData); // так как объект может содержать еще какие-то внутренности, то заного запускаем эту же функцию, только уже с вложенными данными, дальше функция смотрит массив или нет, и если нет - повторяет все еще раз.
+            // Если же функция опять наткнулась на объект - все повторяется
+            // Если вложенные данные оказались массивом, то она просто высчитывает эти данные в первом if, возвращает их в виде [total, data.length] в переменную subDataArra
+
+            total[0] += subDataArray[0]; // записываем эти данные в переменную тотал
+            total[1] += subDataArray[1];
+        }
+
+        return total; // возвращаем все данные
+    }
+}
+
+const result = getTotalProgressByRecursion(students)
+console.log(result[0]/result[1]);
+
+
+
+
+
+// <---------------------------------------------------------------->
+// Примеры:
+// 1) перебор всех свойств объекта
+const data = {
+    "first_level": {
+        "UUU": [ 
+            {
+                "fafsag": 454853450,
+                "zxvxcv": "sag",
+                "DER": {
+                    "YTE": {
+                        "value": "300"
+                    },
+                    "SDSD": {
+                        "value": "WGSHDFH"
+                    },
+                    "ADA": {
+                        "value": "6336736"
+                    }
+                },
+                "mnvfd": "H",
+                "mnxcfg": 25463656
+            }
+        ],
+        "EEE": [
+            {
+                "pohkhl": "2",
+                "lljjklhlh": 4562366,
+                "phhgkg": {
+                    "SDFHGSDH": {
+                        "value": "J"
+                    }
+                },
+                "nvmncnb": "CTRYT",
+                "asfaags": "B",
+                "cncnjsbx": 57567834
+            }
+        ]
+    },
+    "sdghsh": "754747477",
+    "aewtwrte": "CGDGHFJ"
+}
+
+let currentObject = [];
+
+getObjectValue(data);
+
+function getObjectValue(object) {
+    getLower(object);
+
+    function getLower(obj) {
+        for(var prop in obj) {
+            console.log(`${obj} - ${obj[prop]}`)
+            if(typeof(obj[prop]) === 'object') {
+                getLower(obj[prop]);
+            } else {
+                
+            }
+        }
+    }
+}
+https://ru.stackoverflow.com/questions/839617/%D0%A0%D0%B5%D0%BA%D1%83%D1%80%D1%81%D0%B8%D0%B2%D0%BD%D1%8B%D0%B9-%D0%BF%D0%B5%D1%80%D0%B5%D0%B1%D0%BE%D1%80-%D0%B2%D0%BB%D0%BE%D0%B6%D0%B5%D0%BD%D0%BD%D1%8B%D1%85-%D0%BE%D0%B1%D1%8A%D0%B5%D0%BA%D1%82%D0%BE%D0%B2-%D0%BD%D0%B0-javascript
+
+
+// 2) нахождение пути но нужного объекта
+const data1 = {
+  first_level: {
+    UUU: [
+      {
+        fafsag: 454853450,
+        zxvxcv: "sag",
+        DER: [
+          {
+            SDSD: {
+              value: "WGSHDFH"
+            },
+            ADA: {
+              value: "6336736"
+            }
+          },
+          {
+            YTE: {
+              value: "300"
+            }
+          }
+        ],
+        mnvfd: "H",
+        mnxcfg: 25463656
+      }
+    ],
+    EEE: [
+      {
+        pohkhl: "2",
+        lljjklhlh: 4562366,
+        phhgkg: {
+          SDFHGSDH: {
+            value: "J"
+          }
+        },
+        nvmncnb: "CTRYT",
+        asfaags: "B",
+        cncnjsbx: 57567834
+      }
+    ]
+  },
+  sdghsh: "754747477",
+  aewtwrte: "CGDGHFJ"
+};
+
+function find(obj, item, val) {
+  for (const key in obj) {
+    if (obj[key] && typeof obj[key] === "object") {
+      const result = find(obj[key], item, val);
+
+      if (result) {
+        result.unshift(key);
+        return result;
+      }
+    }
+
+    if (key === item && obj[key] === val) {
+      return [obj];
+    }
+  }
+}
+
+function findFormatted(obj, item, val) {
+  const path = find(obj, item, val).slice(0, -1);
+  return path === null ? "" : `['${path.join("']['")}']`;
+}
+
+console.log(find(data1, "value", "300"));
+console.log("path:", findFormatted(data1, "value", "300"));
+console.log(data1["first_level"]["UUU"]["0"]["DER"]["1"]["YTE"]);
+    
---
+++ Practice/L48.Практика, ч6. Используем события на странице проекта/js/script.js
@@ -0,0 +1,108 @@
+/* Задания на урок:
+
+1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
+новый фильм добавляется в список. Страница не должна перезагружаться.
+Новый фильм должен добавляться в movieDB.movies.
+Для получения доступа к значению input - обращаемся к нему как input.value;
+P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
+
+2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
+
+3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
+
+4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
+"Добавляем любимый фильм"
+
+5) Фильмы должны быть отсортированы по алфавиту */
+
+'use strict';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
+    
+    const adv = document.querySelectorAll('.promo__adv img'),
+          genre = document.querySelector('.promo__genre'),
+          bg = document.querySelector('.promo__bg'),
+          movieList = document.querySelector('.promo__interactive-list'),
+          addForm = document.querySelector('.add'),
+          addInput = addForm.querySelector('.adding__input'),
+          deleteFilm = document.querySelectorAll('.delete'),
+          addChecked = addForm.querySelector('[type="checkbox"]');
+
+
+    const deleteAdv = () => {
+        adv.forEach( item => {
+            item.remove();
+        });
+    }
+    
+    const makeChanges = () =>{
+        genre.textContent = 'Драма';
+        bg.style.background = 'url("img/bg.jpg") center center/cover no-repeat';
+    };
+
+    const sortArr = (arr) => {
+        arr.sort();
+    }
+
+    function AddMovies () {
+        movieList.innerHTML = '';
+
+        movieDB.movies.sort().forEach ( (item, i) => {
+            movieList.innerHTML += `<li class="promo__interactive-item">${i+1}: ${item}
+            <div class="delete"></div>
+            </li>`;
+        });
+
+        document.querySelectorAll('.delete').forEach((btn, i) => {
+            btn.addEventListener('click', () => {
+                btn.parentElement.remove();
+                movieDB.movies.splice(i, 1);
+                AddMovies();
+            });
+        });
+
+    }
+
+    
+
+
+
+
+    addForm.addEventListener('submit', (e)=> {
+        e.preventDefault();
+        
+        let newFilm = addInput.value,
+            checked = addChecked.value;
+
+        if (newFilm.length > 21) {
+            newFilm = `${newFilm.substring(0, 22)}...`;
+        }
+
+        if (checked){
+            console.log('Добавляем любимый фильм');
+        }
+
+        movieDB.movies.push(newFilm);
+        AddMovies();
+
+        addForm.target.reset();
+    });
+
+    deleteAdv();
+    makeChanges();
+    AddMovies();
+
+
+});
